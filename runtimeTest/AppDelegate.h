44e1f60d8b7b8a0f03a88336c92cf8842de5a952
Monstar <Monstar@monstarmacbook-pro.local>
2016-08-01 14:58:47 +0800

    Initial Commit

---
+++ runtimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtimeTest
+//
+//  Created by Monstar on 16/8/1.
+//  Copyright © 2016年 Monstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtimeTest
+//
+//  Created by Monstar on 16/8/1.
+//  Copyright © 2016年 Monstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
